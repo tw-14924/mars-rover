5098b19919b281da55ea5456c781597204f2139a
nix <222544972+tw-14924@users.noreply.github.com>
2026-02-09 16:39:51 +0800

    refactor: change parsePlateauLimits to take in an unknown parameter instead

--- src/parsers/plateauLimitParser.ts
+++ src/parsers/plateauLimitParser.ts
@@ -1,16 +1,11 @@
 import type { Coordinates } from "../definitions";
 import { parseCoordinate } from "./parserUtils";
 
-export const parsePlateauLimits = (
-    plateauLimitsRow: unknown[],
-): Coordinates => {
-    if (!Array.isArray(plateauLimitsRow))
-        throw new Error("plateau limits: input should be an array");
+export const parsePlateauLimits = (plateauLimitsRow: unknown): Coordinates => {
+    if (!Array.isArray(plateauLimitsRow)) throw new Error("plateau limits: input should be an array");
 
     if (plateauLimitsRow.length !== 2)
-        throw new Error(
-            `plateau limits: plateauLimitsRow should have 2 values, but got ${plateauLimitsRow.length}`,
-        );
+        throw new Error(`plateau limits: plateauLimitsRow should have 2 values, but got ${plateauLimitsRow.length}`);
 
     const [rawX, rawY] = plateauLimitsRow;
 
--- tests/parsers/plateauLimitParser.test.ts
+++ tests/parsers/plateauLimitParser.test.ts
@@ -5,53 +5,34 @@ describe("parsePlateauLimits", () => {
     describe("happy path", () => {
         test("should return a Coordinate object if given a valid input array", () => {
             expect(parsePlateauLimits([5, 5])).toEqual([5, 5]);
+            expect(parsePlateauLimits([5, "5"])).toEqual([5, 5]);
         });
     });
 
     describe("unhappy path", () => {
         test("should throw if input is not an array", () => {
-            expect(() => parsePlateauLimits("a" as any)).toThrow(
-                "input should be an array",
-            );
-            expect(() => parsePlateauLimits({} as any)).toThrow(
-                "input should be an array",
-            );
+            expect(() => parsePlateauLimits("a" as any)).toThrow("input should be an array");
+            expect(() => parsePlateauLimits({} as any)).toThrow("input should be an array");
         });
 
         test("should throw if plateauLimitsRow is not of size 2", () => {
-            expect(() => parsePlateauLimits([1])).toThrow(
-                "plateauLimitsRow should have 2 values",
-            );
-            expect(() => parsePlateauLimits([1, 3, 4])).toThrow(
-                "plateauLimitsRow should have 2 values",
-            );
-            expect(() => parsePlateauLimits([1, 3, "A"])).toThrow(
-                "plateauLimitsRow should have 2 values",
-            );
+            expect(() => parsePlateauLimits([1])).toThrow("plateauLimitsRow should have 2 values");
+            expect(() => parsePlateauLimits([1, 3, 4])).toThrow("plateauLimitsRow should have 2 values");
+            expect(() => parsePlateauLimits([1, 3, "A"])).toThrow("plateauLimitsRow should have 2 values");
         });
 
         test("should throw if plateauLimitsRow has one or more empty coordinates", () => {
-            expect(() => parsePlateauLimits(["", 1])).toThrow(
-                "coordinate should be non-empty",
-            );
-            expect(() => parsePlateauLimits([1, " "])).toThrow(
-                "coordinate should be non-empty",
-            );
+            expect(() => parsePlateauLimits(["", 1])).toThrow("coordinate should be non-empty");
+            expect(() => parsePlateauLimits([1, " "])).toThrow("coordinate should be non-empty");
         });
 
         test("should throw if plateauLimitsRow contains non-integer coordinates", () => {
-            expect(() => parsePlateauLimits([1, "a"])).toThrow(
-                "coordinate should be an integer",
-            );
-            expect(() => parsePlateauLimits([1, 1.5])).toThrow(
-                "coordinate should be an integer",
-            );
+            expect(() => parsePlateauLimits([1, "a"])).toThrow("coordinate should be an integer");
+            expect(() => parsePlateauLimits([1, 1.5])).toThrow("coordinate should be an integer");
         });
 
         test("should throw if input array contains negative numbers", () => {
-            expect(() => parsePlateauLimits([1, -1])).toThrow(
-                "coordinate should be positive",
-            );
+            expect(() => parsePlateauLimits([1, -1])).toThrow("coordinate should be positive");
         });
     });
 });
