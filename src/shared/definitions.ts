3d7af498ecbcd66f0574233766a0dddca8a6bbea
nix <222544972+tw-14924@users.noreply.github.com>
2026-02-23 11:44:25 +0800

    refactor: update PositionData, RoverData, and InstructionSet to interface

--- src/shared/definitions.ts
+++ src/shared/definitions.ts
@@ -1,6 +1,7 @@
 type RawInput = unknown[][];
-
 type Coordinates = [number, number];
+type FinalPosition = [number, number, Direction];
+type ProcessedResults = FinalPosition[];
 
 const Direction = {
     NORTH: "N",
@@ -10,40 +11,28 @@ const Direction = {
 } as const;
 type Direction = (typeof Direction)[keyof typeof Direction];
 
-type PositionData = {
-    coordinates: Coordinates;
-    direction: Direction;
-};
-
 const Instruction = {
     LEFT: "L",
     RIGHT: "R",
     MOVE: "M",
 } as const;
-
 type Instruction = (typeof Instruction)[keyof typeof Instruction];
 
-type RoverData = {
+interface PositionData {
+    coordinates: Coordinates;
+    direction: Direction;
+}
+
+interface RoverData {
     position: PositionData;
     instructions: Instruction[];
-};
+}
 
-type InstructionSet = {
+interface InstructionSet {
     plateauLimits: Coordinates;
     rovers: RoverData[];
-};
-
-type FinalPosition = [number, number, Direction];
-
-type ProcessedResults = FinalPosition[];
+}
 
 export { Direction, Instruction };
 
-export type {
-    RawInput,
-    Coordinates,
-    PositionData,
-    RoverData,
-    InstructionSet,
-    ProcessedResults,
-};
+export type { RawInput, Coordinates, PositionData, RoverData, InstructionSet, ProcessedResults };
